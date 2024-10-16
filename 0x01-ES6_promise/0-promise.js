6887cdfa2d955f2fc2f2dbc10d7d03c972145521
rmanwa <Robamanwa@gmail.com>
2024-10-16 09:09:24 +0000

    ES6 PROMISES

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -3,10 +3,10 @@ export default function getResponseFromAPI() {
     setTimeout(() => {
       const success = true;
       if (success) {
-        resolve("Data received successfully");  // Resolve the promise with the result
+        resolve("Data received successfully");
       } else {
-        reject("Error: Failed to get response from API");  // Reject the promise with an error
+        reject("Error: Failed to get response from API");
       }
-    }, 1000);  // Simulating a delay of 1 second
+    }, 1000);
   });
 }
