d0bc5e42eb0c004ddcd9af3839e59428d2de1c6e
rmanwa <Robamanwa@gmail.com>
2024-10-21 11:48:12 +0000

    ES6 BASICS

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (const idx in array) {
-    let value = array[idx];
-    array[idx] = appendString + value;
+  const Array = [];
+  for (const value of array) {
+    Array.push(appendString + value);
   }
 
-  return array;
+  return Array;
 }
